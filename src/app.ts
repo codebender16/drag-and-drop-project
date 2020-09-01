426e535323fa0b2d274ee9e3dec8e340a19d7044
Kuan <angusl.zanshin@gmail.com>
2020-09-01 19:38:36 +1000

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+
+  constructor() {
+    this.templateElement = document.getElementById('project-input')!;
+    this.hostElement = 
+
+  }
+}
